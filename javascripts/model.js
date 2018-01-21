0d287c66ee71283188a3cd3bbdb34df36776be5a
Griffin <griffin@Griffins-Air.attlocal.net>
2018-01-20 19:53:36 -0600

    added validation for error messages on login and signup, added field for error in html and small stylings

--- javascripts/model.js
+++ javascripts/model.js
@@ -3,8 +3,18 @@
 const firebase = require("./config/fbConfig");
 const cred = require('./config/fbCreds');
 const $ = require('jquery');
-
 require("firebase/auth");
+
+
+
+let  $txtEmail = $('#txtEmail');
+let  $txtPassword = $('#txtPassword');
+let  $btnLogin = $('#btnLogin');
+let  $btnSignUp = $('#btnSignUp');
+let  $btnLogOut = $('#btnLogOut');
+let $errorArea = $('#errorArea');
+
+
 
 (function doTheThing() {
 
@@ -12,29 +22,28 @@ require("firebase/auth");
   // I had to comment this out for it to work. I am not sure why???
   //firebase.initializeApp(config);
 
-  const $txtEmail = $('#txtEmail');
-  const $txtPassword = $('#txtPassword');
-  const $btnLogin = $('#btnLogin');
-  const $btnSignUp = $('#btnSignUp');
-  const $btnLogOut = $('#btnLogOut');
-
-
   $btnLogin.on('click', e => {
-    const email = $txtEmail.val();
-    const pass = $txtPassword.val();
-    const auth = firebase.auth();
+    $errorArea.empty();
+    let email = $txtEmail.val();
+    let pass = $txtPassword.val();
+    let auth = firebase.auth();
 
     const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
+    promise.catch(function(error){
+      authError(error);
+    });
   });
 
   $btnSignUp.on('click', e => {
+    $errorArea.empty();
     const email = $txtEmail.val();
     const pass = $txtPassword.val();
     const auth = firebase.auth();
 
     const promise = auth.createUserWithEmailAndPassword(email, pass);
-    // promise.catch(authError(error));
+    promise.catch(function(error){
+      authError(error);
+    });
   });
 
   $btnLogOut.on('click', e => {
@@ -44,7 +53,7 @@ require("firebase/auth");
 
   firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
-      // window.alert(`Welcome ${firebaseUser.email}`);
+      // window.alert(`Welcome ${firebas eUser.email}`);
       console.log(firebaseUser.email);
       $('#main-div').addClass('hidden');
       $btnLogOut.removeClass('hidden');
@@ -60,11 +69,20 @@ require("firebase/auth");
 
 }());
 
-// function authError(error){
-//   console.log(error);
-//   console.log('here now');
-// if(error.message =='The email address is badly formatted'){
-//   $('#txtEmail').val('This email is not acceptable, please enter a real email');
-// }
-
-// }
+function authError(error) {
+  console.log(error.message);
+  let errMessage = error.message;
+  switch (errMessage) {
+    case 'The email address is badly formatted.':
+      if($txtEmail.val() ==''){
+        $errorArea.html('You must enter your Email Address');
+      }else if($txtPassword.val() == ''){
+        $errorArea.html('You must enter a password');
+      }else{
+        $errorArea.html('This email is not acceptable, please enter a real email');
+      }
+      break;
+    case 'The password must be 6 characters long or more.':
+      $errorArea.html(error);
+  }
+}
