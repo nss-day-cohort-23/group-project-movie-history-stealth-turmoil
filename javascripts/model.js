10c51d4ee54da89aac1c6e019332cd1d0f4f7750
Griffin <griffin@Griffins-Air.attlocal.net>
2018-01-20 21:32:35 -0600

    added a button for new account, functionality to show username field on signup, added db url to config object, added functionality in signup function to add user w/ name and email to db, added additional error validation

--- javascripts/model.js
+++ javascripts/model.js
@@ -7,19 +7,16 @@ require("firebase/auth");
 
 
 
-let  $txtEmail = $('#txtEmail');
-let  $txtPassword = $('#txtPassword');
-let  $btnLogin = $('#btnLogin');
-let  $btnSignUp = $('#btnSignUp');
-let  $btnLogOut = $('#btnLogOut');
+let $txtEmail = $('#txtEmail');
+let $txtPassword = $('#txtPassword');
+let $btnLogin = $('#btnLogin');
+let $btnSignUp = $('#btnSignUp');
+let $btnLogOut = $('#btnLogOut');
 let $errorArea = $('#errorArea');
-
-
+let $newAccount = $('#newAccount');
 
 (function setListeners() {
 
-  // I had to comment this out for it to work. I am not sure why???
-  //firebase.initializeApp(config);
 
   $btnLogin.on('click', e => {
     $errorArea.empty();
@@ -28,36 +25,39 @@ let $errorArea = $('#errorArea');
     let auth = firebase.auth();
 
     const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.catch(function(error){
+    promise.catch(function (error) {
       authError(error);
     });
   });
-
+  $newAccount.click(function () {
+    $('.login').addClass('hidden');
+    $('.signup').removeClass('hidden');
+  });
   $btnSignUp.on('click', e => {
     $errorArea.empty();
     const email = $txtEmail.val();
     const pass = $txtPassword.val();
     const auth = firebase.auth();
-
-    const promise = auth.createUserWithEmailAndPassword(email, pass);
-    promise.catch(function(error){
+    const displayName = $('userName').val();
+    const promise = auth.createUserWithEmailAndPassword(email, pass)
+      .then(authData => {
+        let $userName = $('#userName').val();
+        firebase.database().ref("users").child(authData.uid).set({ name: $userName, email: authData.email });
+        window.alert(`Welcome ${$userName}`);
+      });
+    promise.catch(function (error) {
       authError(error);
     });
   });
 
   $btnLogOut.on('click', e => {
-    console.log('click');
     firebase.auth().signOut();
   });
 
   firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
-      // window.alert(`Welcome ${firebas eUser.email}`);
-      console.log(firebaseUser.email);
       $('#main-div').addClass('hidden');
       $btnLogOut.removeClass('hidden');
-
-
     } else {
       console.log('not logged in');
       $btnLogOut.addClass('hidden');
@@ -73,15 +73,20 @@ function authError(error) {
   let errMessage = error.message;
   switch (errMessage) {
     case 'The email address is badly formatted.':
-      if($txtEmail.val() ==''){
+      if ($txtEmail.val() == '') {
         $errorArea.html('You must enter your Email Address');
-      }else if($txtPassword.val() == ''){
+      } else if ($txtPassword.val() == '') {
         $errorArea.html('You must enter a password');
-      }else{
+      } else {
         $errorArea.html('This email is not acceptable, please enter a real email');
       }
       break;
     case 'The password must be 6 characters long or more.':
       $errorArea.html(error);
+      break;
+    case 'The email address is already in use by another account.':
+      $errorArea.html('There is already an account associated with this email address.');
+      break;
+
   }
 }
