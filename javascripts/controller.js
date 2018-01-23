d708eee32dd38171d4cc4ca84bd74b1f77f6eabd
Trey Mosley <iiimosley@Treys-MacBook-Pro.local>
2018-01-23 09:05:04 -0600

    initial page - flex styling

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -5,61 +5,11 @@ const model = require('./model');
 const view = require('./view');
 
 
-function timedPrint(movies, time){
-    //within a certain time frame...
-    setTimeout(function(){
-        //prints each movie of sub array to the DOM
-        movies.forEach(m => $("#output").append(`${m} `));
-    //offsets each print by factoring the time w/ index
-    //3*1,3*2,3*3
-    }, 3000 * time);
-}
-
-function movieSplit (movies, splitIndex) {
-    let splitArr = [];
-    //loop through remaining movies
-    for (let i = 0; i<movies.length; i+=splitIndex) {
-        //splices at each index of x40 to its own array
-        let subArr = movies.slice(i, i+splitIndex);
-        //pushes each of those 40ct arrays into a parent array
-        splitArr.push(subArr);
-    }
-    console.log(splitArr);
-    //loop sends each child-array to the timing function w/ timing factor
-    for (let j = 0; j < splitArr.length; j++) {
-        timedPrint(splitArr[j], (j + 1));
-    }
-}
-
-function bundleData() {
-    let restMovies = [];
-    // i = each movie     130: dummy data for # of total movies
-    for(let i=0; i<130; i++) {
-        //print first 39 movies w/actors 
-        //// 1 call (all movies) + 39 calls (each movie actors) = 40 calls
-        if (i<39) {
-            $("#output").append(`${i} `);
-        }
-        //set rest of movies to array
-        else {
-            restMovies.push(i);
-        }
-    }
-    //send rest of movies to timed release function, plus number to slice array
-    movieSplit(restMovies, 40);
-}
-
-// bundleData();
-
-
-
-// let allMovies = [];
-
-function getMoreMovies(page){
+function getMoreMovies(page, searchText){
 // start setTimeout for each API request
-    setTimeout(function () {
+    let timer = setTimeout(function () {
         //gets API data to page# given
-        model.getMovies(page)
+        model.getMovies(page, searchText)
             //prints each movies to the DOM
             .then((movieData) => {
                 movieData.results.sort((a, b) => b.popularity - a.popularity);
@@ -68,11 +18,15 @@ function getMoreMovies(page){
                         .then(creditsData => view.printMovie(m, creditsData.cast));
                     });
                 });  //sends requests in 10 sec increments
-            }, 10000 * (page-1));
+            }, 10500 * (page-1));            
+    $("#movieSearch").on("click", function() {
+        clearTimeout(timer);
+        // setTimeout(loadEnter(), 5000);
+    });  
 }
 
-
-function initialMovies (firstMovies, totalPages) {
+function initialMovies (firstMovies, totalPages, searchText) {
+    console.log(firstMovies);
     //sorts first set of movies by popularity
     firstMovies.sort((a,b)=>b.popularity - a.popularity);
     //sends each movie to be printed to DOM
@@ -82,12 +36,30 @@ function initialMovies (firstMovies, totalPages) {
     });
     //sends each remaining page number to setTimout function
     for (let i=2;i<(totalPages+1);i++){
-        getMoreMovies(i);
+        getMoreMovies(i, searchText);
     }
 }
 
-//fetches page 1 (20movies); 
-//sends those movies plus total #pages
-model.getMoviesInit()
-    .then(movieData=>initialMovies(movieData.results, movieData.total_pages));
-//     .then((initData) => moreMovies(initData.results, initData.total_pages));
+
+function loadEnter() {
+    $("#movieSearch").keypress(function(){
+        if (event.keyCode == 13) {
+            let searchText = $(this).val();
+            $(this).val('');
+            $("#output").empty();
+            //fetches page 1 (20movies); 
+            //sends those movies plus total #pages & search text
+            model.getMoviesInit(searchText)
+                .then(movieData=>initialMovies(movieData.results, movieData.total_pages, searchText));
+            // $("#movieSearch").val("");
+        }
+    });
+}
+
+function loadPage() {
+    loadEnter();
+    model.getTopMovies()
+        .then((topMovies)=>view.printTopMovies(topMovies));
+}
+
+loadPage();
--- javascripts/model.js
+++ javascripts/model.js
@@ -3,10 +3,21 @@
 const $ = require('jquery');
 
 
+module.exports.getTopMovies = () => {
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            'url': 'https://api.themoviedb.org/3/movie/top_rated?api_key=5d0e04db35c01aaf8be4d1fc3f7e47c6&language=en-US&page=1'
+        })
+            .done((topMovies) => {
+                resolve(topMovies);
+            });
+    });
+};
+
 module.exports.getCast = (movieID) => {
     return new Promise(function (resolve, reject) {
         $.ajax({
-            "url": `https://api.themoviedb.org/3/movie/${movieID}/credits?api_key=b7770e2d95281d16626611ba20512744`
+            "url": `https://api.themoviedb.org/3/movie/${movieID}/credits?api_key=5d0e04db35c01aaf8be4d1fc3f7e47c6`
         })
             .done((castData) => {
                 resolve(castData);
@@ -14,11 +25,10 @@ module.exports.getCast = (movieID) => {
     });
 };
 
-
-module.exports.getMovies = (i) => {
+module.exports.getMovies = (i, searchVal) => {
     return new Promise(function (resolve, reject) {
         $.ajax({
-            "url": `https://api.themoviedb.org/3/search/movie?include_adult=false&query=%27war%27&page=${i}&language=en-US&api_key=b7770e2d95281d16626611ba20512744`
+            "url": `https://api.themoviedb.org/3/search/movie?include_adult=false&query=%27${searchVal}%27&page=${i}&language=en-US&api_key=5d0e04db35c01aaf8be4d1fc3f7e47c6`
         })
         .done((movieData) => {
             resolve(movieData);
@@ -26,15 +36,14 @@ module.exports.getMovies = (i) => {
     });
 };
 
-module.exports.getMoviesInit = () => {
+module.exports.getMoviesInit = (searchVal) => {
     return new Promise(function (resolve, reject) {
         $.ajax({
-            "url": "https://api.themoviedb.org/3/search/movie?include_adult=false&query=%27war%27&language=en-US&api_key=b7770e2d95281d16626611ba20512744"
+            "url": `https://api.themoviedb.org/3/search/movie?include_adult=false&query=%27${searchVal}%27&language=en-US&api_key=5d0e04db35c01aaf8be4d1fc3f7e47c6`
         })
         .done((initData) => {
             resolve(initData);
         });
     });
 };
-
 
